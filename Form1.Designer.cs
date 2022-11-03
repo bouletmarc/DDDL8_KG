bcc635d8c932cec0a823d12f97e44f281b145598
Bouletmarc <bouletmarc@hotmail.com>
2022-11-03 09:49:11 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace DDDL8_Keygen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(188, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Apply level10 access to DDDL8";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(211, 44);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "DDDL8 - Keygen (BMDevs)";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DDDL8_Keygen
+{
+    public partial class Form1 : Form
+    {
+        private string Path1 = @"C:\ProgramData\Detroit Diesel\Drumroll\Application Data\";
+        private string Filena = "Settings.xml";
+        private string FilenaBACKUP = "SettingsBACKUP.xml";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Path1 = Application.StartupPath + "\\";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(Path1 + Filena))
+            {
+                string[] AllLines = File.ReadAllLines(Path1 + Filena);
+                List<string> SaveLines = new List<string>();
+
+                for (int i = 0; i < AllLines.Length; i++)
+                {
+                    if (AllLines[i].Contains("RegistrationKey</Property>"))
+                    {
+                        int Counting = 0;
+                        while (Counting < 9)
+                        {
+                            SaveLines.Add(AllLines[i]);
+                            Counting++;
+                            i++;
+                        }
+                        SaveLines.Add("<Object type=\"DetroitDiesel.Settings.SettingInformation, CommonCS\">");
+                        SaveLines.Add("    <Property name=\"Name\">ReadLevel</Property>");
+                        SaveLines.Add("    <Property name=\"Group\">Client</Property>");
+                        SaveLines.Add("    <Property name=\"Obj\">");
+                        SaveLines.Add("      <Object type=\"System.Int32, mscorlib\">10</Object>");
+                        SaveLines.Add("    </Property>");
+                        SaveLines.Add("    <Property name=\"Encrypt\">False</Property>");
+                        SaveLines.Add("</Object>");
+                        SaveLines.Add("<Object type=\"DetroitDiesel.Settings.SettingInformation, CommonCS\">");
+                        SaveLines.Add("    <Property name=\"Name\">WriteLevel</Property>");
+                        SaveLines.Add("    <Property name=\"Group\">Client</Property>");
+                        SaveLines.Add("    <Property name=\"Obj\">");
+                        SaveLines.Add("      <Object type=\"System.Int32, mscorlib\">10</Object>");
+                        SaveLines.Add("   </Property>");
+                        SaveLines.Add("    <Property name=\"Encrypt\">False</Property>");
+                        SaveLines.Add("</Object>");
+                        SaveLines.Add("<Object type=\"DetroitDiesel.Settings.SettingInformation, CommonCS\">");
+                        SaveLines.Add("   <Property name=\"Name\">ComputerDescription</Property>");
+                        SaveLines.Add("   <Property name=\"Group\">Client</Property>");
+                        SaveLines.Add("    <Property name=\"Obj\">");
+                        SaveLines.Add("      <Object type=\"DetroitDiesel.Settings.StringSetting, CommonCS\">");
+                        SaveLines.Add("        <Property name=\"Value\">BMDevs</Property>");
+                        SaveLines.Add("      </Object>");
+                        SaveLines.Add("   </Property>");
+                        SaveLines.Add("    <Property name=\"Encrypt\">False</Property>");
+                        SaveLines.Add("</Object>");
+
+                        SaveLines.Add(AllLines[i]);
+                    }
+                    else
+                    {
+                        SaveLines.Add(AllLines[i]);
+                    }
+                }
+
+
+                //Remake
+                string[] AllLinesFinal = new string[SaveLines.Count];
+                for (int i = 0; i < AllLinesFinal.Length; i++) AllLinesFinal[i] = SaveLines[i];
+
+                File.Create(Path1 + FilenaBACKUP).Dispose();
+                File.WriteAllBytes(Path1 + FilenaBACKUP, File.ReadAllBytes(Path1 + Filena));
+
+                File.Create(Path1 + Filena).Dispose();
+                File.WriteAllLines(Path1 + Filena, AllLinesFinal);
+
+                MessageBox.Show("Done!");
+            }
+            else
+            {
+                MessageBox.Show("Cannot find file: '" + Path1 + Filena + "'");
+            }
+        }
+    }
+}
